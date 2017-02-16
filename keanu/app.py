9154e16efc5ce95a174453e65f8335190036b801
aagavin <aagavin@gmail.com>
2017-02-16 15:22:02 -0500

    login api

--- keanu/app.py
+++ keanu/app.py
@@ -1,15 +1,21 @@
 from flask import Flask, jsonify
 from keanu.routes import login
+from flask_autodoc import Autodoc
 
 flask_app = Flask(__name__)
+auto = Autodoc(flask_app)
 
-# flask_app.register_blueprint(spec_api)
 flask_app.register_blueprint(login.login_api)
 
 
 @flask_app.route('/spec')
 def spec():
-    return jsonify({})
+    return auto.html()
+
+
+@flask_app.route('/test')
+def applol():
+    return jsonify({'data': 'success'})
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
--- keanu/routes/login.py
+++ keanu/routes/login.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, jsonify
+from flask_autodoc import Autodoc
 
 login_api = Blueprint('login_api', __name__)
+from ..app import flask_app
+auto = Autodoc(flask_app)
+
+
+@login_api.route('/login/spc')
+def login_doc():
+    return auto.html()
 
 
 @login_api.route('/login')
+@auto.doc()
 def login() -> dict:
     """
     Login to the api
