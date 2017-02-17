ebd2a47ba4690d8313aecb3681266896e0ebb520
aagavin <aagavin@gmail.com>
2017-02-17 12:48:17 -0500

    test remove setup

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_packages
 setup(name="KeanuFromBehind",
       description="Backend end for keanu's frontend",
       author="",
-      packages=find_packages(),
-      requires=['gunicorn', 'flask']
+      packages=find_packages()
 )
