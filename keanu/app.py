95cddc292f985e2b7c67f920ccbb92a5fa1b4200
Aaron <aagavin@gmail.com>
2017-02-17 10:03:49 -0500

    test remove deps in setup

--- keanu/app.py
+++ keanu/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify
 from flask_autodoc import Autodoc
-
 from keanu.routes.login import login_api
 
 flask_app = Flask(__name__)
@@ -10,16 +9,24 @@ flask_app.register_blueprint(login_api)
 auto = Autodoc(flask_app)
 
 
-@flask_app.route('/')
-@auto.doc()
-def applol():
-    return jsonify({'data': 'success'})
-
-
-@flask_app.route('/spec')
+@flask_app.route('/spec', methods=['GET'])
 def spec():
+    """
+    View spec for root API endpoints
+    :return:
+    """
     return auto.html()
 
 
+@flask_app.route('/', methods=['GET'])
+@auto.doc()
+def root():
+    """
+    Root api to test if its working
+    :return:
+    """
+    return jsonify({'data': {'success': True}})
+
+
 if __name__ == "__main__":
-    flask_app.run(debug=False)
+    flask_app.run(debug=True)
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name="KeanuFromBehind",
       description="Backend end for keanu's frontend",
       author="",
-      packages=find_packages(),
-      install_requires=['flask', 'gunicorn', 'Flask-Autodoc']
+      packages=find_packages()
+      # install_requires=['flask', 'gunicorn', 'Flask-Autodoc']
       )
