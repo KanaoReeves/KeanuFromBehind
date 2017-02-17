8ff3c6ecf4fc29575e156a44cf346ec3daf0e722
Aaron <aagavin@gmail.com>
2017-02-16 23:13:48 -0500

    .

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-
-from setuptools import setup
-
-setup(name="KeanuFromBehind",
-      description="Backend end for keanu's frontend",
-      author="",
-      packages=['keanu']
-      )
+#
+# from setuptools import setup
+#
+# setup(name="KeanuFromBehind",
+#       description="Backend end for keanu's frontend",
+#       author="",
+#       packages=['keanu']
+#       )
