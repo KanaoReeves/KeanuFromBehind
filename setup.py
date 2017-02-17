cf0e9a655f85ed0fa8aa9bbe0ffa37af2b29192f
Aaron <aagavin@gmail.com>
2017-02-16 22:25:51 -0500

    fixed spelling

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
 from setuptools import setup
 
-setup(name="KeanuFromBehine",
+setup(name="KeanuFromBehind",
       description="Backend end for keanu's frontend",
       author="",
-      
+
       )
