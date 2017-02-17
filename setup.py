bcc8b229b13a0dbf1d2842f629da5191f4db75e2
Aaron <aagavin@gmail.com>
2017-02-17 11:48:53 -0500

    added new dep
    gunicorn dep

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(name="KeanuFromBehind",
       description="Backend end for keanu's frontend",
       author="",
-      packages=find_packages()
+      packages=find_packages(),
+      requires=['gunicorn']
 )
