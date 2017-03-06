fba02101d31f8ede483dc953056ae9d138411a98
Arlina Ramrattan <arlina.ramrattan@gmail.com>
2017-03-06 09:59:40 -0500

    added unit test for items

--- keanu/models/orders.py
+++ keanu/models/orders.py
@@ -9,17 +9,3 @@ class Orders(db.Document):
     userId = db.IntField(required=True)
     date = db.DateTimeField(required=True)
 
-
-class Tags(db.Document):
-    id = db.IntField(required=True)
-    tag = db.StringField(required=True)
-
-
-class Items(db.Document):
-    id = db.IntField(required=True)
-    name = db.StringField(required=True)
-    description = db.StringField(max_length=150, required=True)
-    price = db.DecimalField(required=True)
-    calories = db.IntField(required=True)
-    category = db.StringField(required=True)
-    tags = db.ListField(Tags, required=True)
---
+++ tests/item_test.py
@@ -0,0 +1,42 @@
+import unittest
+
+from keanu.app import flask_app
+from keanu.models.Items import Items, Tags
+
+
+class TestItems(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Setup app for testing
+        :return:
+        """
+       # self.app = flask_app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        """
+        Remove app after testing
+        :return:
+        """
+        self.app.delete()
+
+    def test_app_exists(self):
+        self.assertFalse(self.app is None)
+
+    def test_add_new_item(self):
+        new_item = Item(
+            id='8888',
+            name='Test Item',
+            description='This is just a test description',
+            price=9.99,
+            calories=500,
+            category='Entrees',
+            tags=['bread', 'healthy']
+        )
+
+
+        new_item.save()
+        found_item = Items.query.filter(Items.id == new_item.id).first()
+        self.assertEqual(new_item.id, found_item.id, "Items not equal")
+        new_item.remove()
