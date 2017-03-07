2902d0e06741230c4e7a55522ebfcb1bd4413e9d
Aaron <aagavin@gmail.com>
2017-03-06 22:35:53 -0500

    added get item by id and category

--- keanu/app.py
+++ keanu/app.py
@@ -32,7 +32,7 @@ def before_request() -> tuple:
     :return:
     """
     from keanu.models.users import User
-    no_auth_paths = ['/', '/spec', '/favicon.ico', '/item', '/login', '/login/register']
+    no_auth_paths = ['/', '/spec', '/favicon.ico', '/item', '/item/58be0265f188127b8fd2af52', '/login', '/login/register']
     if request.path not in no_auth_paths and 'token' in request.headers:
         token = request.headers['token']
         user = User.query.filter(User.token == token).first()
--- keanu/routes/items.py
+++ keanu/routes/items.py
@@ -19,7 +19,7 @@ def login_doc():
 
 @item_api.route('/item', methods=['GET'])
 @auto.doc()
-def get_all_items() -> tuple:
+def get_all_items() -> dict:
     """
     returns all the items as a json array
     :return:
@@ -27,6 +27,48 @@ def get_all_items() -> tuple:
     from keanu.models.items import Item
     # get all items
     items = Item.query.all()
+    # create items list
+    items_list = []
+    # create response
+    for item in items:
+        items_list.append({
+            "_id": str(item.mongo_id),
+            "name": item.name,
+            "description": item.description,
+            "imageURL": item.imageURL,
+            "price": item.price,
+            "calories": item.calories,
+            "category": item.category,
+            "tags": item.tags
+        })
+    return jsonify({'data': {'items': items_list}})
+
+
+@item_api.route('/item/id/<id>', methods=['GET'])
+@auto.doc()
+def get_item_by_id(id) -> tuple:
+    from keanu.models.items import Item
+    # find specific item
+    item = Item.query.filter(Item.mongo_id == id).first()
+    item_json = {
+        "_id": str(item.mongo_id),
+        "name": item.name,
+        "description": item.description,
+        "imageURL": item.imageURL,
+        "price": item.price,
+        "calories": item.calories,
+        "category": item.category,
+        "tags": item.tags
+    }
+    return jsonify({'data': {'item': item_json}})
+
+
+@item_api.route('/item/category/<category>', methods=['GET'])
+@auto.doc()
+def get_item_by_category(category) -> tuple:
+    from keanu.models.items import Item
+    # find items by category
+    items = Item.query.filter(Item.category == category)
     # create items list
     items_list = []
     # create response
