4d3e5467cb4913735bd3cf9ae1841483f352de72
Aaron <aagavin@gmail.com>
2017-03-21 10:54:29 -0400

    added test for add item for admin

--- tests/item_test.py
+++ tests/item_test.py
@@ -40,3 +40,19 @@ class TestItemRoute(unittest.TestCase):
         result = self.app.get('/item/category/Starter')
         json_data = json.loads(result.data)
         self.assertTrue(json_data['data']['items'] is not None, 'no item by id found')
+
+    def test_admin_add_item(self):
+        data= '{"name": "testItem101",	"description": "this is a description",	"imageURL": "https://example.com",' \
+              '	"price": 33.95,	"calories": 500, "category": "Starter", "tags": ["asdf", "sdfsdf", "sdfs"]}'
+        # loin as admin and get admin token
+        login_result = self.app.post('/login', headers={'username': 'aaron', 'password': 'password'})
+        admin_token = json.loads(login_result.data)['data']['token']
+        # add item as admin
+        result = self.app.post(
+            '/admin/item/add',
+            headers={'Content-Type': 'application/json', 'token': admin_token},
+            data=data
+        )
+
+        json_data = json.loads(result.data)
+        self.assertTrue(json_data['data']['item'] is not None, 'item not added')
