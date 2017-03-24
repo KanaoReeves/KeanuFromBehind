60d081fb559809d4780db847285e694229bedd1d
aagavin <aagavin@gmail.com>
2017-03-23 21:57:29 -0400

    fixed db model

--- keanu/models/orders.py
+++ keanu/models/orders.py
@@ -1,13 +1,18 @@
 from keanu.app import flask_db as db
 
 
+class ItemQuantity(db.Document):
+    itemId = db.StringField(required=True)
+    quantity = db.IntField(required=True)
+
+
 class Order(db.Document):
     config_collection_name = 'orders'
 
-    items = db.ListField(db.ListField(db.StringField()), required=True)
+    items = db.ListField(db.DocumentField(ItemQuantity), required=True)
     total = db.FloatField(required=True)
     userId = db.StringField(required=True)
     delivery = db.BoolField(required=True)
-    date = db.FloatField(required=True)
+    date = db.StringField(required=True)
 
 
