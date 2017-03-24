8bfe2d665781ad756fb4fc84244f9367815409be
aagavin <aagavin@gmail.com>
2017-03-23 21:58:15 -0400

    added working ordet tes

--- tests/order_test.py
+++ tests/order_test.py
@@ -2,6 +2,7 @@ import unittest
 import json
 
 from keanu.app import flask_app
+from keanu.models.orders import Order
 
 
 class TestOrderRoute(unittest.TestCase):
@@ -23,21 +24,25 @@ class TestOrderRoute(unittest.TestCase):
     def test_app_exists(self):
         self.assertFalse(self.app is None)
 
-    def test_add_new_order_route(self):
+    def test_add_new_order(self):
         login = self.app.post('/login', headers={'username': 'steve', 'password': 'smith'})
         json_login = json.loads(login.data)
-        data = '{"date": "1.4896756223270907E9", "delivery": "True", "items": [["123", "5"], ["25", "2"], ["33", "1"]], ' \
-               '"total": "29.99", "userId": "58bda399c2e2222840edddb2"}'
+        data = '{"date": "12:05:2017","delivery": true,"items": [' \
+               '{"58cb056f98717507c730d78b": 5},{"58cb0577630cc007c7870772": 2},' \
+               '{"58d13f3e63ae2507c70a7d5e": 1}' \
+               '],"price": 25.06}'
 
         result = self.app.post('/order/add', headers={'token': json_login['data']['token']}, data=data, content_type='application/json')
         json_response = json.loads(result.data)
-        self.assertEqual(json.dumps(json_response['data']['orders']), data, 'data returned is not the same')
+        self.assertIsNotNone(json_response['data']['orderId'], 'error in response')
+        added_order = Order.query.get(json_response['data']['orderId'])
+        added_order.remove()
 
-    def test_get_user_orders(self):
-        login = self.app.post('/login', headers={'username': 'steve', 'password': 'smith'})
-        json_response = json.loads(login.data)
-        if json_response['data']['token'] is not None:
-             result = self.app.get('/order', headers={
-                'token': json_response['data']['token']})
-             json_data = json.loads(result.data)
-             self.assertTrue(len(json_data['data']['orders']) is not None, "no orders in db")
+    # def test_get_user_orders(self):
+    #     login = self.app.post('/login', headers={'username': 'steve', 'password': 'smith'})
+    #     json_response = json.loads(login.data)
+    #     if json_response['data']['token'] is not None:
+    #          result = self.app.get('/order', headers={
+    #             'token': json_response['data']['token']})
+    #          json_data = json.loads(result.data)
+    #          self.assertTrue(len(json_data['data']['orders']) is not None, "no orders in db")
